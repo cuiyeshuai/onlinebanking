1e6eac98425533c89d2085e04332acd62465a027
Jiaqing Nie <jiaqing.nie@kcl.ac.uk>
2020-11-19 16:50:45 +0800

    Test the basic functionality of login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,17 +2,22 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get login" do
-    get sessions_login_url
+    get '/login'
     assert_response :success
   end
 
-  test "should get verification" do
-    get sessions_verification_url
-    assert_response :success
+  test "should redirect_to success page with correct username and password" do
+    post '/login', params: {username:"Jiaqing", password:"123456"}
+    assert_redirected_to(controller: 'sessions', action: 'success')
+  end
+
+  test "should redirect_to login page with incorrect username or password" do
+    post '/login', params: {username:"Jiaqing", password:"123"}
+    assert_redirected_to(controller: 'sessions', action: 'login')
   end
 
   test "should get success" do
-    get sessions_success_url
+    get '/success'
     assert_response :success
   end
 
