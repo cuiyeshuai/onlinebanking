e842d0916776b5065ba536c35804fdd789b557a1
Justinas Kiskis <justinas.kiskis@kcl.ac.uk>
2020-11-30 21:08:27 +0000

    Add tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get dummy page fees" do
+    get '/pages/fees'
+    assert_response :success
+  end
+
+  test "should get dummy page invest" do
+    get '/pages/invest'
+    assert_response :success
+  end
+
 end
