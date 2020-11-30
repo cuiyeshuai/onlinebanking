3544924ef997d2ff852830b4923f44c633ec1260
Ork Hossain Muntaqin <ork.hossain.muntaqin@kcl.ac.uk>
2020-11-30 16:20:07 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'l_page', to:'l_page#index'
   get 'login', to: 'sessions#login'
   post 'login', to: 'sessions#verification'
-  get 'success', to: 'sessions#success'
+  get 'success', to: 'l_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
