63f738411900eb4225cb6c2184a6db8b45321534
Cuiyeshuai <k1924221@kcl.ac.uk>
2020-12-10 17:53:31 +0800

    T9 User should be able to interact with transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,26 @@
 class TransactionsController < ApplicationController
   def index
+    @transactions = Transaction.order('date DESC')
   end
-
   def show
+    @transaction = Transaction.find(params[:id])
   end
 
   def new
+    @transaction = Transaction.new
   end
 
   def create
+    params.require(:transaction).permit(:amount, :recipient_name, :recipient, :reference)
+    @transaction = Transaction.new(params[:transaction])
+    @transaction.currency = params[:currency]
+    @transaction.date = DateTime.current()
+    @transaction.updated_at = @transaction.created_at = Time.now()
+    if @transaction.save 
+      redirect_to(transactions_path)
+    else 
+      render('new')
+    end
   end
 
   
