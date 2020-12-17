3a4fe33eddc2c4cb9b3aead450d762967865aac1
Cuiyeshuai <k1924221@kcl.ac.uk>
2020-12-18 02:30:30 +0800

    User integration tests finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
       def downcase_username()
         self.username = username.downcase
       end
-
+      def User.digest(passphrase)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(passphrase, cost: cost)
+      end
+      # source: https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
     has_many :bank_accounts, :dependent => :destroy
 end
