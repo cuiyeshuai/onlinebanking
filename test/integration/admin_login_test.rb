0989c04920e47b224d93873230472c40ca3342f5
Cuiyeshuai <k1924221@kcl.ac.uk>
2020-12-18 13:43:26 +0800

    Add integration testing for administrator login

---
+++ test/integration/admin_login_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class AdminLoginTest < ActionDispatch::IntegrationTest
+  test "login with valid credential" do
+    admin = Administrator.new({administratorname: "liujianlong", password: "123456", password_confirmation: "123456"})
+    admin.save
+    get admin_new_url
+    assert_response :success
+    assert_not admin_is_logged_in?
+    assert_template 'admin/sessions/new'
+    post admin_new_url, params: { administratorname: "liujianlong", password: "123456"}
+    assert_redirected_to '/admin/dashboard'
+    follow_redirect!
+    assert_template 'admin/dashboard/index'
+    assert admin_is_logged_in?
+  end
+  test "login with invalid credential" do
+    admin = Administrator.new({administratorname: "liujianlong", password: "123456", password_confirmation: "123456"})
+    admin.save
+    get admin_new_url
+    assert_response :success
+    assert_not admin_is_logged_in?
+    assert_template 'admin/sessions/new'
+    post admin_new_url, params: { administratorname: "liujianlong", password: "1234567"}
+    assert_template 'admin/sessions/new'
+    assert_not admin_is_logged_in?
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,11 +8,15 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :users, :bank_accounts, :transactions
+  fixtures :administrators, :users, :bank_accounts, :transactions
 
   # Add more helper methods to be used by all tests here...
   def user_is_logged_in?
     !session[:user_id].nil?
+  end
+
+  def admin_is_logged_in?
+    !session[:administrator_id].nil?
   end
 
 end
