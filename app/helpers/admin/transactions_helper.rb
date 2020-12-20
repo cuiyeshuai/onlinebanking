0c4abbd6749cc5c7f20bab4cf7f8afb90a7c0e50
Jiaqing Nie <jiaqing.nie@kcl.ac.uk>
2020-12-21 02:42:09 +0800

    finish the functionality of alert and notification

--- app/helpers/admin/transactions_helper.rb
+++ app/helpers/admin/transactions_helper.rb
@@ -1,27 +1,27 @@
 module Admin::TransactionsHelper
-  
+
     def generate_transactions(amount,user,bankacc)
-        amount/2.times{
+      (amount/2).times{
         t = Transaction.new
         t.currency = BankAccount.find(bankacc).currency
         t.amount = Faker::Number.decimal(l_digits: 2, r_digits: 2)
-        t.recipient = User.find(user).first_name + User.find(user).last_name 
+        t.recipient = User.find(user).first_name + User.find(user).last_name
         t.recipient_account = bankacc
-        t.reference = Faker::Restaurant.name  
+        t.reference = Faker::Restaurant.name
         t.remitter_account = Faker::Bank.account_number(digits: 8)
-        t.remitter = Faker::Restaurant.name  
+        t.remitter = Faker::Restaurant.name
         t.save
-        }
-        amount/2.times{
-            t = Transaction.new
-            t.currency = BankAccount.find(bankacc).currency
-            t.amount = Faker::Number.decimal(l_digits: 2, r_digits: 2)
-            t.recipient = Faker::Company.name  
-            t.recipient_account = Faker::Bank.account_number(digits: 8)
-            t.reference = Faker::Company.name  
-            t.remitter_account = bankacc
-            t.remitter = User.find(user).first_name + User.find(user).last_name
-            t.save
-            }
+      }
+      (amount/2).times{
+        t = Transaction.new
+        t.currency = BankAccount.find(bankacc).currency
+        t.amount = Faker::Number.decimal(l_digits: 2, r_digits: 2)
+        t.recipient = Faker::Company.name
+        t.recipient_account = Faker::Bank.account_number(digits: 8)
+        t.reference = Faker::Company.name
+        t.remitter_account = bankacc
+        t.remitter = User.find(user).first_name + User.find(user).last_name
+        t.save
+      }
     end
 end
