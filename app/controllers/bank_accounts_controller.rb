fffe204a4abb8cc978f0ee468706f67a66849411
Jiaqing Nie <jiaqing.nie@kcl.ac.uk>
2020-12-22 14:17:09 +0800

    add the charts into both user and administrator pages

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,10 +1,12 @@
 class BankAccountsController < UserController
-
+  include SessionsHelper
   def index
+    @user = current_user
     @bankAccounts = BankAccount.where(user_id: session[:user_id])
   end
 
   def show
+
     @bankAccount = BankAccount.find(params[:id])
     # Save chosen bank account
     set_account(@bankAccount)
--- app/helpers/admin/transactions_helper.rb
+++ app/helpers/admin/transactions_helper.rb
@@ -5,7 +5,8 @@ module Admin::TransactionsHelper
         t = Transaction.new
         t.currency = BankAccount.find(bankacc).currency
         t.amount = Faker::Number.decimal(l_digits: 2, r_digits: 2)
-        t.recipient = User.find(user).first_name + User.find(user).last_name
+        u = User.find(user)
+        t.recipient = u.first_name + ", " + u.last_name
         t.recipient_account = bankacc
         t.reference = Faker::Restaurant.name
         t.remitter_account = Faker::Bank.account_number(digits: 8)
@@ -20,7 +21,8 @@ module Admin::TransactionsHelper
         t.recipient_account = Faker::Bank.account_number(digits: 8)
         t.reference = Faker::Company.name
         t.remitter_account = bankacc
-        t.remitter = User.find(user).first_name + User.find(user).last_name
+        u = User.find(user)
+        t.remitter = u.first_name+ ", " + u.last_name
         t.save
       }
     end
