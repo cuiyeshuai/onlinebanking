3aebb814aa97448ad5ace287c3390c96853a351d
Jiaqing Nie <jiaqing.nie@kcl.ac.uk>
2020-12-22 19:52:17 +0800

    fix the display bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,28 +15,36 @@ class PagesController < ApplicationController
         render template: "pages/welcomepage"
     end
 
+    # Get current exchange rates from API and store in sessions
     def currency_rates
       if session[:currency].nil?
         session[:currency] = {}
-        res0 = JSON.parse(HTTParty.get("https://free.currconv.com/api/v7/convert?q=GBP_USD,GBP_EUR&compact=ultra&apiKey=a367d690ea1d8b90c1d5").to_json)
-        res1 = JSON.parse(HTTParty.get("https://free.currconv.com/api/v7/convert?q=GBP_CNY,GBP_JPY&compact=ultra&apiKey=a367d690ea1d8b90c1d5").to_json)
-        res2 = JSON.parse(HTTParty.get("https://free.currconv.com/api/v7/convert?q=GBP_RUB,GBP_HKD&compact=ultra&apiKey=a367d690ea1d8b90c1d5").to_json)
+        res = JSON.parse(HTTParty.get("https://api.exchangeratesapi.io/latest?base=GBP").to_json)
 
-        session[:currency][:g2u] = res0["GBP_USD"]
-        session[:currency][:g2e] = res0["GBP_EUR"]
-        session[:currency][:g2c] = res1["GBP_CNY"]
-        session[:currency][:g2j] = res1["GBP_JPY"]
-        session[:currency][:g2r] = res2["GBP_RUB"]
-        session[:currency][:g2h] = res2["GBP_HKD"]
 
+        session[:currency][:g2u] = res["rates"]["USD"].round(2)
+        session[:currency][:g2e] = res["rates"]["EUR"].round(2)
+        session[:currency][:g2c] = res["rates"]["CNY"].round(2)
+        session[:currency][:g2j] = res["rates"]["JPY"].round(2)
+        session[:currency][:g2r] = res["rates"]["RUB"].round(2)
+        session[:currency][:g2h] = res["rates"]["HKD"].round(2)
+
+        @g2u = res["rates"]["USD"].round(2)
+        @g2e = res["rates"]["EUR"].round(2)
+        @g2c = res["rates"]["CNY"].round(2)
+        @g2j = res["rates"]["JPY"].round(2)
+        @g2r = res["rates"]["RUB"].round(2)
+        @g2h = res["rates"]["HKD"].round(2)
+
+      else
+        puts session[:currency]
+        @g2u = session[:currency]["g2u"]
+        @g2e = session[:currency]["g2e"]
+        @g2c = session[:currency]["g2c"]
+        @g2j = session[:currency]["g2j"]
+        @g2r = session[:currency]["g2r"]
+        @g2h = session[:currency]["g2h"]
       end
-      puts session[:currency]
-      @g2u = session[:currency]["g2u"]
-      @g2e = session[:currency]["g2e"]
-      @g2c = session[:currency]["g2c"]
-      @g2j = session[:currency]["g2j"]
-      @g2r = session[:currency]["g2r"]
-      @g2h = session[:currency]["g2h"]
 
     end
 
